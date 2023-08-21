15349b86b4205d5dd08757146f405376a423a921
kurzkopfgleitbeutler <einfachzuverstehen@posteo.de>
2023-08-21 15:55:11 +0200

    add template file. Baseline used for developing new scripts

---
+++ template.jl
@@ -0,0 +1,40 @@
+#!/usr/bin/env julia
+module TODO
+include("util.jl")
+
+@doc """
+$name - TODO
+
+# Options
+
+- ? -? --? -h --help Show this screen.
+- -v --verbose       Show on terminal what's happening during execution.
+- -l --log           Write log to file.
+
+# Usage
+
+$name [OPTIONS] [target device] filename [file names]
+
+# Known Limitations
+""" name
+
+# dependencies = [""]
+# optionals = [""]
+
+mutable struct states
+  demandoptionals::Bool
+  loglvl::LogLevel
+  logtofile::Bool
+  states() = new(true, Logging.Warn, false)
+end
+
+# state = init(getflags([...]))
+state = init()
+function main()
+
+
+
+end
+main()
+end
+@time eval(TODO)
